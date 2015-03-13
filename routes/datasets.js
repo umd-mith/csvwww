c3d250ccc6727b2e22d6c1738c718feb033df97e
Ed Summers <ehs@pobox.com>
2015-03-13 16:50:17 -0400

    tabs

--- routes/datasets.js
+++ routes/datasets.js
@@ -7,13 +7,13 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/:id', function(req, res, next) {
-	Dataset.findById(req.params.id, function(err, dataset) {
-		if (dataset) {
-			res.render('dataset', {dataset: dataset});	
-		} else {
-			res.send(404);
-		}
-	});
+  Dataset.findById(req.params.id, function(err, dataset) {
+    if (dataset) {
+      res.render('dataset', {dataset: dataset});
+    } else {
+      res.send(404);
+    }
+  });
 });
 
 module.exports = router;
